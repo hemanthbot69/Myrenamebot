99125235d3823d0e02d02f6fcb7fb027d4f0e3d1
hemanthbot69 <104711832+hemanthbot69@users.noreply.github.com>
2023-07-03 22:55:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,20 +26,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "27917732")
+    API_HASH  = os.environ.get("API_HASH", "1e9e5064624862f0181fe42b821e4e1a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6391655955:AAGVLOLeGMnVY6wZchd7GwHgrAK4IzAZLFQ") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://tabefaf971:7CYhZY0cz13xSrbP@cluster0.bjqhdes.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001240752200") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001699482807"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
